9a646e6097f3b805ac6b84ef2faa7b8251af915a
Jonathan Roitgrund <jroitgrund@gmail.com>
2016-04-27 20:19:46 +0100

    Add deduplication, mark URLs seen first.

--- src/lib/crawl.js
+++ src/lib/crawl.js
@@ -2,17 +2,30 @@ import urlLib from 'url';
 
 const sameDomain = (link1, link2) => urlLib.parse(link1).host === urlLib.parse(link2).host;
 
+const removeProtocol = url => {
+  const parsedUrl = urlLib.parse(url);
+  parsedUrl.protocol = '';
+  return parsedUrl.format();
+};
+
 export default fetchLinksAndAssetsAndAssets => rootUrl => {
   const urlMap = {};
+  const seenUrls = {};
   const recursiveFetch = url => fetchLinksAndAssetsAndAssets(url)
     .then(linksAndAssets => {
+      console.log(url);
+
       if (linksAndAssets.links.length > 0 || linksAndAssets.assets.length > 0) {
         urlMap[url] = linksAndAssets;
       }
 
       return Promise.all(
         linksAndAssets.links
-          .filter(link => urlMap[link] === undefined && sameDomain(rootUrl, link))
+          .filter(link => !seenUrls[removeProtocol(link)] && sameDomain(rootUrl, link))
+          .map(link => {
+            seenUrls[removeProtocol(link)] = true;
+            return link;
+          })
           .map(recursiveFetch));
     }); // No need to catch, we can just silently stop crawling for that URL.
 
--- src/lib/fetch-links-and-assets.js
+++ src/lib/fetch-links-and-assets.js
@@ -2,25 +2,41 @@ import cheerio from 'cheerio';
 import urlLib from 'url';
 
 const getAbsolutePathForTagAndAttribute = ($, url, elementType, attribute) =>
-  $(elementType).map((i, el) => urlLib.resolve(url, $(el).attr(attribute))).get();
+  $(elementType).map((i, el) => {
+    if ($(el).attr(attribute)) {
+      const rawUrl = urlLib.resolve(url, $(el).attr(attribute));
+      const parsedUrl = urlLib.parse(rawUrl);
+      parsedUrl.hash = '';
+      return parsedUrl.format();
+    } else {
+      return undefined;
+    }
+  }).get().filter(url => url); // Filters out undefined;
+
+const uniq = array => array.filter((item, i, arr) => arr.indexOf(item) === i);
 
 export default requestPromise => url => requestPromise
   .head(url)
   .then(headers => {
-    if (headers['content-type'] !== undefined &&
-         (headers['content-type'] === 'text/html' ||
-          headers['content-type'] === 'application/xhtml+xml')) {
-      return requestPromise.get(url)
-        .then(cheerio.load)
-        .then($ => {
-          const links = getAbsolutePathForTagAndAttribute($, url, 'a', 'href');
-          const assets = getAbsolutePathForTagAndAttribute($, url, 'img', 'src').concat(
-            getAbsolutePathForTagAndAttribute($, url, 'link', 'rel'),
-            getAbsolutePathForTagAndAttribute($, url, 'script', 'src')
-          );
-          return { links: links.sort(), assets: assets.sort() };
-        });
-    }
+    try {
+      if (headers['content-type'] !== undefined &&
+           (headers['content-type'].includes('text/html') ||
+            headers['content-type'].includes('application/xhtml+xml'))) {
+        return requestPromise.get(url)
+          .then(cheerio.load)
+          .then($ => {
+            const links = getAbsolutePathForTagAndAttribute($, url, 'a', 'href');
+            const assets = getAbsolutePathForTagAndAttribute($, url, 'img', 'src').concat(
+              getAbsolutePathForTagAndAttribute($, url, 'link', 'rel'),
+              getAbsolutePathForTagAndAttribute($, url, 'script', 'src')
+            );
+            return { links: uniq(links).sort(), assets: uniq(assets).sort() };
+          })
+          .catch(() => ({ links: [], assets: [] }));
+      }
 
-    return { links: [], assets: [] };
+      return { links: [], assets: [] };
+    } catch (ignored) {
+      return { links: [], assets: [] }; // This catches bugs in the http request library.
+    }
   });
